e0db5bcba6a48f72cad27d001ba46852eac12506
Jaideep <jaideep@tokbox.com>
2014-03-07 16:37:15 -0800

    just a backup

--- ConnectivityDoctor/ConnectivityDoctor/WelcomeViewController.m
+++ ConnectivityDoctor/ConnectivityDoctor/WelcomeViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "WelcomeViewController.h"
+#import "AppDelegate.h"
 
 @interface WelcomeViewController ()
-
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @end
 
 @implementation WelcomeViewController
@@ -26,9 +27,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
+    
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    self.managedObjectContext = appDelegate.managedObjectContext;
 
+    [self startNetworkOperation];
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -36,7 +41,43 @@
 }
 
 - (IBAction)unwindToWelcome:(UIStoryboardSegue *)segue {
-    NSLog(@"UNWIND");
+    
 }
 
+#pragma mark Network
+-(void) startNetworkOperation
+{
+ 
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://sup301-sat.tokbox.com/dynamicTestConfig.json"]
+                                                              cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                          timeoutInterval:10];
+    [urlRequest setHTTPMethod: @"GET"];
+
+    [NSURLConnection sendAsynchronousRequest:urlRequest queue:[[NSOperationQueue alloc] init] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error)
+    {
+        if ([data length] > 0 && error == nil) {
+            NSDictionary * info = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            NSDictionary * servers = [info objectForKey:@"servers"];
+            
+            NSManagedObject * newManagedObject;
+            int i =0;
+            for (NSString* key in servers) {
+                
+                newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:@"DiagnosticGroup"
+                                                                 inManagedObjectContext:self.managedObjectContext];
+                [newManagedObject setValue:@(i++) forKey:@"type"];
+                [newManagedObject setValue:key forKey:@"name"];
+            }
+            
+            NSLog(@"%@",servers);
+        }
+        
+        else if ([data length] == 0 && error == nil)
+             NSLog(@"NTWK:got no data");
+        else if (error != nil && error.code == NSURLErrorTimedOut)
+             NSLog(@"NTWK:timeout");
+        else if (error != nil)
+             NSLog(@"NTWK:error %@",error.description);
+    }];
+}
 @end
