1d532b5660db69784d2f64ae9f065e599acaf547
Jaideep <jaideep@tokbox.com>
2014-03-14 16:08:22 -0700

    group cell because of progress view customization and potentially put network operations here

---
+++ ConnectivityDoctor/ConnectivityDoctor/GroupCell.m
@@ -0,0 +1,29 @@
+//
+//  GroupCell.m
+//  ConnectivityDoctor
+//
+//  Created by Jaideep Shah on 3/14/14.
+//  Copyright (c) 2014 Jaideep Shah. All rights reserved.
+//
+
+#import "GroupCell.h"
+
+@implementation GroupCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
