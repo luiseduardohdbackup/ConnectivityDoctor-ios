d96803cbb3da29017752d17e583e4c52415d4058
Jaideep <jaideep@tokbox.com>
2014-03-25 16:07:26 -0700

    pdf added for detailed report

---
+++ ConnectivityDoctor/ConnectivityDoctor/SendReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendReportViewController.h
+//  ConnectivityDoctor
+//
+//  Created by Jaideep Shah on 3/20/14.
+//  Copyright (c) 2014 Jaideep Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendReportViewController : UIViewController
+
+@end
